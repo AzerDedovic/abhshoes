ba7434286ae4dd9b59143b0cbfe2793e867376e1
AzerDedovic <adedovic2@etf.unsa.ba>
2016-06-06 15:34:53 +0200

    stripe configuration6

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -5,7 +5,7 @@ end
 def create
   # Amount in cents
   
-  @amount = amount
+  #@amount = amount
 
   customer = Stripe::Customer.create(
     :email => params[:stripeEmail],
@@ -14,7 +14,7 @@ def create
 
   charge = Stripe::Charge.create(
     :customer    => customer.id,
-    :amount      => @amount,
+    :amount      => @total *100  ,
     :description => 'Rails Stripe customer',
     :currency    => 'bam'
   )
