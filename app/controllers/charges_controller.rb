93d4022bbec8550530680483c4db3661672027fb
AzerDedovic <adedovic2@etf.unsa.ba>
2016-06-14 12:04:48 +0200

    order

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -9,6 +9,10 @@ def create
   @user=current_user
   @total = params[:amount]
   @amount = @total.to_i
+  @cart_id=Cart.find_by(user_id: @user.id).id
+  @delivery_address=Delivery.where(cart_id: @cart_id).order("created_at").last
+  @billing_address=Bill.where(cart_id: @cart_id).order("created_at").last
+
 
   customer = Stripe::Customer.create(
     :email => params[:stripeEmail],
@@ -21,6 +25,12 @@ def create
     :description => 'Rails Stripe customer',
     :currency    => 'bam'
   )
+  @order=Order.new()
+  @order.user_id=@user.id
+  @order.total_price=(@total.to_f/100)
+  @order.delivery_id=@delivery_address.id
+  @order.bill_id=@billing_address.id
+  @order.save
 
 rescue Stripe::CardError => e
   flash[:error] = e.message
