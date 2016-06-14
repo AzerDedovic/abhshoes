ba36f2fc491570cf2462c77f037b779f935c5ad4
AzerDedovic <adedovic2@etf.unsa.ba>
2016-06-14 13:15:53 +0200

    charges_helper

--- app/helpers/charges_helper.rb
+++ app/helpers/charges_helper.rb
@@ -1,2 +1,27 @@
 module ChargesHelper
+	def CreateOrderItems(order_id)
+		@order_id=order_id
+		@user=current_user
+		@cart_id=Cart.find_by(user_id: @user.id).id
+
+		@items=CartItem.where(cart_id: @cart_id).order(created_at: :desc).to_a
+
+		@items.each do |item|
+			@order_item=OrderItem.new()
+
+			@order_item.name=Product.find_by(id: item.product_id).name
+			@order_item.price=Product.find_by(id: item.product_id).price
+			@order_item.image=Product.find_by(id: item.product_id).image
+			@order_item.size=item.size
+			@order_item.color=item.color
+			@order_item.quantity=item.quantity
+			@order_item.order_id=@order_id
+
+			@order_item.save
+
+		end
+
+		CartItem.destroy_all(cart_id: @cart_id)
+
+	end
 end
