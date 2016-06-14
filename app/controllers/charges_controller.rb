622cc814a6b26973b32e64191bb53eba25892eac
AzerDedovic <adedovic2@etf.unsa.ba>
2016-06-14 13:37:09 +0200

    order_items_fix

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -34,7 +34,27 @@ def create
 
   @order_id=Order.where(user_id: @user.id).order("created_at").last
 
-  createOrderItems
+  @cart_id=Cart.find_by(user_id: @user.id).id
+  @items=CartItem.where(cart_id: @cart_id).order(created_at: :desc).to_a
+
+
+
+  @items.each do |item|
+      @order_item=OrderItem.new()
+
+      @order_item.name=Product.find_by(id: item.product_id).name
+      @order_item.price=Product.find_by(id: item.product_id).price
+      @order_item.image=Product.find_by(id: item.product_id).image
+      @order_item.size=item.size
+      @order_item.color=item.color
+      @order_item.quantity=item.quantity
+      @order_item.order_id=@order_id
+
+      @order_item.save
+
+    end
+  CartItem.destroy_all(cart_id: @cart_id)
+
 
 rescue Stripe::CardError => e
   flash[:error] = e.message
