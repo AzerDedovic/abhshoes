2618e0e360f80159ae61fcbcdc99669f1b558dd0
AzerDedovic <azer@tests-Macintosh-2.local>
2016-07-26 16:21:57 +0200

    recurrence

--- app/workers/newsletter_worker.rb
+++ app/workers/newsletter_worker.rb
@@ -2,7 +2,8 @@ class NewsletterWorker
   include Sidekiq::Worker
   include Sidetiq::Schedulable
 
-  recurrence { hourly.minute_of_hour(0, 10, 20, 30, 40, 50) }
+  #recurrence { hourly.minute_of_hour(0, 10, 20, 30, 40, 50) }
+  recurrence { hourly(6) }
 
   def perform()
   	@users=User.where(email_confirmed:true)
